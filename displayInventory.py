5367f272791c0ac1b6915efbfeef35db4a6fb2d7
Austin Loyd <austin@Austins-Air.lan>
2023-05-07 16:02:14 -0500

    for statement

--- displayInventory.py
+++ displayInventory.py
@@ -1,7 +1,16 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
-def displayInventory(inventory):
-    # your code goes here
+def displayInventory(stuff):
+    
+    total_items = 0
+
+    print('Inventory: ')
+    for item, amount in stuff.items():
+        print(amount,item)
+        total_items += amount
+
+    print(f'Total number of items: {total_items}')
+
 
 
 if __name__ == "__main__":
